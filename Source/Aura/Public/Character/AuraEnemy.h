f7bc241799116540955e9c938bad9cac07e3e107
Anuj Pisalkar <anujpisalkar@gmail.com>
2024-08-30 11:53:39 +0530

    Section 2 Done

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -4,14 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacter.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacter
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 	
+public:
+	AAuraEnemy();
+	virtual void HighLightActor() override;
+	virtual void UnHighLightActor() override;
 };
